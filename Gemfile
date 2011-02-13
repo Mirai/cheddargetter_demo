c646d4b18589bbdaead3c15fc809f6065746f2c6
Robert Miner <robertrminer@gmail.com>
2011-02-13 04:50:11 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+group :development, :test do
+  gem 'webrat'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'cucumber'
+  gem 'cucumber-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheddargetterDemo::Application.config.secret_token = 'c3a57fdbbbd21d4b5338302384e207dfb5b9b4358a63c8199d9fc78aa6733feea1ccceff24b0b604210287c0259a9d4b98b2815b215e0d738bfd34be4ef23875'
